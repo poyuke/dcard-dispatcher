276ece88ffe82b9df7615598a75f3b3ec08db9ad
py.ke <dk1i6m4@gmail.com>
2021-05-25 03:24:02 +0800

    feat: modify logic

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-contrib/zap v0.0.1
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/onsi/gomega v1.12.0 // indirect
 	github.com/speps/go-hashids/v2 v2.0.1
 	github.com/spf13/viper v1.7.1
 	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
--- pkg/dao/redis.go
+++ pkg/dao/redis.go
@@ -2,9 +2,11 @@ package dao
 
 import (
 	"fmt"
+	"strings"
 	"time"
-	"go.uber.org/zap"
+
 	"github.com/speps/go-hashids/v2"
+	"go.uber.org/zap"
 )
 
 func NewDAO(logger *zap.Logger) *DAO {
@@ -21,17 +23,24 @@ func (e Env) SetRedis(fileContent string) (string, error) {
 	data := make(map[string]interface{})
 	data["fileContent"] = fileContent
 	data["status"] = "waiting"
-	data["sha"] = ""
-	err = e.Redis.HMSet(hashId, data).Err()
+	dcardKey := fmt.Sprintf("dcard-%s", hashId)
+	err = e.Redis.HMSet(dcardKey, data).Err()
 	if err != nil {
 		e.Logger.Error("Redis Error", zap.String("SetRedis", "set file"), zap.String("error", err.Error()))
+		return "", err
+	}
+	scanKey := fmt.Sprintf("scan-%s", hashId)
+	err = e.Redis.Set(scanKey, "0", 0).Err()
+	if err != nil {
+		e.Logger.Error("Redis Error", zap.String("SetRedis", "set scan key"), zap.String("error", err.Error()))
 		return "", err
 	}
 	return hashId, nil
 }
 
 func (e Env) GetRedisByKey(key string, field string) (string, error) {
-	data, err := e.Redis.HMGet(key, field).Result()
+	dcardKey := fmt.Sprintf("dcard-%s", key)
+	data, err := e.Redis.HMGet(dcardKey, field).Result()
 	if err != nil {
 		e.Logger.Error("Redis Error", zap.String("GetRedisByKey", "hmget key"), zap.String("error", err.Error()))
 		return "", err
@@ -44,19 +53,28 @@ func (e Env) GetRedisByKey(key string, field string) (string, error) {
 }
 
 func (e Env) GetRedisAllStatus() (map[string]interface{}, error) {
-	keys, err := e.Redis.Keys("*").Result()
-	if err != nil {
-		e.Logger.Error("Redis Error", zap.String("GetRedisAllStatus", "get keys"), zap.String("error", err.Error()))
-		return nil, err
-	}
 	jobs := make(map[string]interface{})
-	for _, key := range keys {
-		data, err := e.Redis.HMGet(key, "status").Result()
+	var cursor uint64
+	for {
+		var keys []string
+		var err error
+		keys, cursor, err := e.Redis.Scan(cursor, "dcard-*", 20).Result()
 		if err != nil {
-			e.Logger.Error("Redis Error", zap.String("GetRedisAllStatus", "hmget key"), zap.String("error", err.Error()))
+			e.Logger.Error("Redis Error", zap.String("GetRedisAllStatus", "get keys"), zap.String("error", err.Error()))
 			return nil, err
 		}
-		jobs[key] = data[0]
+		for _, key := range keys {
+			data, err := e.Redis.HMGet(key, "status").Result()
+			if err != nil {
+				e.Logger.Error("Redis Error", zap.String("GetRedisAllStatus", "hmget key"), zap.String("error", err.Error()))
+				return nil, err
+			}
+			keyAry := strings.Split(key, "-")
+			jobs[keyAry[1]] = data[0]
+		}
+		if cursor == 0 {
+			break
+		}
 	}
 	return jobs, nil
 }
